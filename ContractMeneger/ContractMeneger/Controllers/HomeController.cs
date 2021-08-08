2213726fa83c20d4098678d4765fa9db71f7d1f7
nickchuchunov <nick.chuchunov@yandex.u>
2021-08-08 19:09:50 +0300

    выполнение 3 задания

--- ContractMeneger/ContractMeneger/Controllers/HomeController.cs
+++ ContractMeneger/ContractMeneger/Controllers/HomeController.cs
@@ -7,31 +7,44 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ContractMeneger.Controllers
+namespace ContractMeneger
 {
+    [Route("api/HomeController")]
+    [ApiController]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private ServicesPerson _ServicesPerson;
+        private ListPersonRepository _ListPersonRepository;
+
+        HomeController(ServicesPerson _ServicesPerson, ListPersonRepository _ListPersonRepository) 
         {
-            _logger = logger;
+
+            this._ServicesPerson = _ServicesPerson;
+            this._ListPersonRepository = _ListPersonRepository;
+
+
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //GET /persons/{id}
+        [HttpGet("/persons/{id}")]
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public Person ControllerGetPersonOfId ([FromRoute] int Id ) { return _ServicesPerson.GetPersonOfId(Id, _ListPersonRepository.Datebase);       }//  получение человека по идентификатору
+        
+        [HttpGet("/persons/search?FirstName={FirstName} ")]
+        public List<Person> ControllerSearhPersonOfFirstName([FromRoute] string FirstName) { return _ServicesPerson.SearhPersonOfFirstName(FirstName, _ListPersonRepository.Datebase); } //  поиск человека по имени
+     
+        [HttpGet("/persons/?skip={5}&take={10}")]
+        public List<Person> ControllerPaginationListPerson() { return _ServicesPerson.PaginationListPerson( _ListPersonRepository.Datebase); }  //  получение списка людей с пагинацией
+    
+        [HttpPut("/persons")]
+        public List<Person> ControllerUpdatePersonList([FromRoute] Person person) { return _ServicesPerson.UpdatePersonList(_ListPersonRepository.Datebase, person); }      //Обновление персоны в коллекции
+                                                                                                                                                                 
+        [HttpDelete("/persons/{Id}")]
+        public List<Person> ControllerRemovalPerson([FromRoute] int Id) { return _ServicesPerson.RemovalPerson(_ListPersonRepository.Datebase, Id); }   // Удаление персоны
+                                                                                                                                           
+        [HttpPost("/persons")]
+        public List<Person> ControllerAddPersonOfList([FromRoute] Person person) { return _ServicesPerson.AddPersonOfList(_ListPersonRepository.Datebase, person); }     //  добавление новой персоны в коллекцию 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ ContractMeneger/ContractMeneger/Services/ServicesPerson.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContractMeneger
+{
+    public class ServicesPerson : IRepositoryPersen<Person>
+    {
+        public Person GetPersonOfId(int Id, List<Person> _person) //  получение человека по идентификатору
+        {
+            int i = Id; return _person[i];
+        }
+
+        public List<Person> PaginationListPerson(List<Person> _person) //  получение списка людей с пагинацией
+        {
+            return _person;
+        }
+
+        public List<Person> RemovalPerson(List<Person> _person, int Id) // Удаление персоны
+        {
+            for (int i=0; i< _person.Count(); i++) { while (_person[i].Id == Id) { _person.Remove(_person[i]);     } }
+
+            return _person;
+        }
+
+        public List<Person> SearhPersonOfFirstName(string FirstName, List<Person> _person) //  поиск человека по имени
+        {
+            List<Person> People = new List<Person>();
+            foreach (Person people in _person) { while (people.FirstName == FirstName) { People.Add(people); } }
+
+
+            return People;
+        }
+
+        public List<Person> UpdatePersonList(List<Person> _person, Person _pipel) //Обновление персоны в коллекции
+        {
+            for (int i = 0; i < _person.Count; i++)
+            {
+                while (_person[i].Id == _pipel.Id)
+                {
+
+                    _person[i].FirstName = _pipel.FirstName; _person[i].LastName = _pipel.LastName; _person[i].Email = _pipel.Email; _person[i].Company = _pipel.Company; _person[i].Age = _pipel.Age;
+                }
+            }
+            return _person;
+
+
+      
+        }
+
+
+        public List<Person> AddPersonOfList(List<Person> _person, Person _pipel) { _person.Add(_pipel); return _person; }
+
+
+
+    }
+
+
+
+
+
+
+}
